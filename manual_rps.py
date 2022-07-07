8ced5fadeb7c033551b62d25a1ef4c437a0bd86f
shah9917 <shahharsha9917@gmail.com>
2022-07-07 18:51:49 +0100

    milestone 3

---
+++ manual_rps.py
@@ -0,0 +1,36 @@
+import random
+
+signs = ['rock','paper','scissor']
+def get_computer_choice(choices):
+    return(random.choice(choices))
+
+def get_user_choice(choices):
+    choice = input("Choose: rock, paper or scissor")
+    if choice not in choices:
+        print('Make valid choice')
+        choice = get_user_choice()
+    return(choice)
+
+def get_winner(computer_choice, user_choice):
+    if computer_choice == 'rock' and user_choice =='scissor':
+        return ('computer')
+    elif computer_choice == 'rock' and user_choice =='paper':
+        return('user')
+    elif computer_choice == 'paper' and user_choice == 'scissor':
+        return('user')
+    elif computer_choice == 'paper' and user_choice == 'rock':
+        return('computer')
+    elif computer_choice == 'scissor' and user_choice == 'rock':
+        return('user')
+    elif computer_choice == 'scissor' and user_choice == 'paper':
+        return('computer')
+    else:
+        return('noone')
+
+def play():
+    computer_choice = get_computer_choice(signs)
+    user_choice = get_user_choice(signs)
+    winner = get_winner(computer_choice, user_choice)
+    print('Computer played ' + computer_choice)
+    print('User played ' + user_choice)
+    return('Winner is ' + winner)
