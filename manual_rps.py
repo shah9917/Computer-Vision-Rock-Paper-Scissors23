0da8805bde5e9748e9b6e2d9c29e853f019ea477
shah9917 <shahharsha9917@gmail.com>
2022-07-18 19:14:02 +0100

    Milestone 4

--- manual_rps.py
+++ manual_rps.py
@@ -8,22 +8,14 @@ def get_user_choice(choices):
     choice = input("Choose: rock, paper or scissor")
     if choice not in choices:
         print('Make valid choice')
-        choice = get_user_choice()
+        choice = get_user_choice(choices)
     return(choice)
 
 def get_winner(computer_choice, user_choice):
-    if computer_choice == 'rock' and user_choice =='scissor':
+    if (computer_choice == 'rock' and user_choice =='scissor') or (computer_choice == 'scissor' and user_choice == 'paper') or (computer_choice == 'paper' and user_choice == 'rock'):
         return ('computer')
-    elif computer_choice == 'rock' and user_choice =='paper':
+    elif (computer_choice == 'rock' and user_choice =='paper') or (computer_choice == 'paper' and user_choice == 'scissor') or (computer_choice == 'scissor' and user_choice == 'rock') :
         return('user')
-    elif computer_choice == 'paper' and user_choice == 'scissor':
-        return('user')
-    elif computer_choice == 'paper' and user_choice == 'rock':
-        return('computer')
-    elif computer_choice == 'scissor' and user_choice == 'rock':
-        return('user')
-    elif computer_choice == 'scissor' and user_choice == 'paper':
-        return('computer')
     else:
         return('noone')
 
@@ -34,3 +26,5 @@ def play():
     print('Computer played ' + computer_choice)
     print('User played ' + user_choice)
     return('Winner is ' + winner)
+
+print(play())
